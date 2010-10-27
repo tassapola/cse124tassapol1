2a91ac3ce59a6b8c3de95716d3ae67dfcbded521
chilltassapola <chilltassapola@d00b6542-4b24-a583-fce2-81e969cd9922>
2010-10-27 06:57:23 +0000

    add util.h

---
+++ http_server/src/util.h
@@ -0,0 +1,16 @@
+/*
+ * util.h
+ *
+ *  Created on: Oct 26, 2010
+ *      Author: torvnc
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+
+#endif /* UTIL_H_ */
+
+struct FirstCmd {
+	char *httpOp, *path, *httpVersion;
+};
