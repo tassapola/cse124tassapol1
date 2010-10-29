14c4d3930cd08747f5b3524afd3110cc70b3fef2
chilltassapola <chilltassapola@d00b6542-4b24-a583-fce2-81e969cd9922>
2010-10-29 03:49:23 +0000

    fix compilation bug; add date to all response; respond with current date; add getgmtdate to util

--- http_server/src/util.c
+++ http_server/src/util.c
@@ -4,7 +4,44 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <time.h>
 
+char *getNowGMTDate() {
+	time_t timer;                // Define the timer
+	struct tm *tblock;           // Define a structure for time block
+    timer = time(NULL);
+    tblock = gmtime(&timer);
+    char *ans = malloc(sizeof(char) * 10000);
+	ans[0] = '\0';
+	strcat(ans, "Date: ");
+	switch(tblock->tm_wday) {
+	  case 0: strcat(ans,"Sun "); break;
+	  case 1: strcat(ans,"Mon "); break;
+	  case 2: strcat(ans,"Tue "); break;
+	  case 3: strcat(ans,"Wed "); break;
+	  case 4: strcat(ans,"Thu "); break;
+	  case 5: strcat(ans,"Fri "); break;
+	  case 6: strcat(ans,"Sat "); break;
+	}
+	sprintf(ans,"%s%d ",ans, tblock->tm_mday);
+	switch(tblock->tm_mon) {
+	  case 0: strcat(ans,"Jan "); break;
+	  case 1: strcat(ans,"Feb "); break;
+	  case 2: strcat(ans,"Mar "); break;
+	  case 3: strcat(ans,"Apr "); break;
+	  case 4: strcat(ans,"May "); break;
+	  case 5: strcat(ans,"Jun "); break;
+	  case 6: strcat(ans,"Jul "); break;
+	  case 7: strcat(ans,"Aug "); break;
+	  case 8: strcat(ans,"Sep "); break;
+	  case 9: strcat(ans,"Oct "); break;
+	  case 10: strcat(ans,"Nov "); break;
+	  case 11: strcat(ans,"Dec "); break;
+	}
+	sprintf(ans,"%s%d ",ans, 1900 + tblock->tm_year);
+	sprintf(ans,"%s%02d:%02d:%02d GMT",ans, tblock->tm_hour, tblock->tm_min, tblock->tm_sec);
+	return ans;
+}
 
 void addCRLF(char *buffer, int *bufferLen) {
 	buffer[*bufferLen] = 13;
